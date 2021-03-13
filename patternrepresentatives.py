ba2c3d8b86bdcaa433169bf9c665bf3ee44674c3
Deranged-Alpacas <derangedalpacas1234@gmail.com>
2021-03-13 14:44:30 -0500

    Fixed pattern generator and added writing to files

--- patternrepresentatives.py
+++ patternrepresentatives.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 # Generating Pattern Representatives
 # The format of the matrices is that each entry is represented by a triple (a,b,c)=(a+b*sqrt(2))/(sqrt(2)^c)
@@ -15,6 +16,7 @@ def transpose(mat):
     for i in range(len(mat[0])):
         vOut.append([mat[j][i] for j in range(len(mat))])
     return vOut
+
 
 # Need to revamp the permutation to also cover row permutations
 def colpermcheck(m):
@@ -144,10 +146,14 @@ def listdisplay(li):
     return listr
 
 def matdisplay(M):
+    mat = ""
     for row in M:
-        print(listdisplay(row))
+        mat = mat + listdisplay(row) + "\n"
+    return mat
 
 def tupmult(t1, t2):
+    if (t1[0] == 0 and t1[1] == 0) or (t2[0] == 0 and t2[1] == 0):
+        return (0,0,0)
     t3 = (t1[0]*t2[0]+2*t1[1]*t2[1], t1[1]*t2[0]+t1[0]*t2[1],t1[2]+t2[2])
     while t3[0]%2 == 0 and t3[2] > 0:
         t3 = (t3[1], int(t3[0]/2), t3[2]-1)
@@ -164,13 +170,15 @@ def tupadd(t1, t2):
         while t1cop[2] > t2cop[2]:
             t2cop = (2*t2cop[1], t2cop[0], t2cop[2]+1)
         t3 = (t1cop[0] + t2cop[0], t1cop[1] + t2cop[1], t2cop[2])
-    while t3[0]%2 == 0 and (t3[0] != 0 and t3[1] != 0):
+    if t3[0] == 0 and t3[1] == 0:
+        return (0,0,0)
+    while t3[0]%2 == 0 and t3[2]>0:
         t3 = (t3[1], int(t3[0]/2), t3[2]-1)
     return t3
 
 def matmult(m1, m2):
     """Multiplies to matrices together"""
-    m3 = noneMat
+    m3 = copy.deepcopy(noneMat)
     for i in range(6):
         for j in range(6):
             m3[i][j] = tupadd(tupmult(m1[i][0],m2[0][j]),tupadd(tupmult(m1[i][1],m2[1][j]),\
@@ -189,27 +197,37 @@ def leastdenompower(m):
 
 def residue(m):
     """Returns the residue of a matrix in SO_6(Z[1/sqrt(2))"""
-    copy = m.copy()
+    cop = copy.deepcopy(m)
     leastpower = leastdenompower(m)
-    for row in copy:
+    for row in cop:
         for entry in row:
             for i in range(leastpower - entry[2]):
                 entry = (2*entry[1], entry[0], entry[2] + 1)
-    residues = noneMat
-    for i in range(len(copy)):
-        for j in range(len(copy[0])):
+    residues = copy.deepcopy(noneMat)
+    for i in range(len(cop)):
+        for j in range(len(cop[0])):
             residues[i][j] = (entry[0]%2, entry[1]%2)
     return residues
 
 def Tmat(i, j):
     """Returns T_{ij} after mapped to SO_6(Z[1/sqrt(2))"""
-    T = zeroMat
-    T[i-1][j-1] = (-1,0,1)
-    T[i-1][i-1], T[j-1][i-1], T[j-1][j-1] = (1,0,1), (1,0,1), (1,0,1)
+    Ttoreturn = copy.deepcopy(zeroMat)
+    Ttoreturn[i-1][j-1] = (-1,0,1)
+    Ttoreturn[i-1][i-1], Ttoreturn[j-1][i-1], Ttoreturn[j-1][j-1] = (1,0,1), (1,0,1), (1,0,1)
     for k in range(0,6):
         if k != i-1 and k != j-1:
-            T[k][k] = (1,0,0)
-    return(T)
+            Ttoreturn[k][k] = (1,0,0)
+    return(Ttoreturn)
+
+
+def fileoutput(m, tcount, string):
+    s = "****************\n\n"
+    s = s + "pattern: \n"
+    s = s + matdisplay(m)+ "\n"
+    s = s + "Tcount: {} \n".format(tcount)
+    s = s + "residue: {} \n".format(residue(m))
+    s = s + "String: {} \n\n".format(string)
+    return s
 
 
 # RELEVANT LISTS
@@ -219,27 +237,26 @@ Tlabels = ["T12","T13","T14","T15","T16","T23","T24","T25","T26","T34","T35","T3
 tcountpatterns = [[],[],[],[],[]]
 allmats = []
 
+
 # GENERATING PATTERNS
 
-for i in range(len(Ts)):
-    tcountpatterns[0].append((Ts[i], 1, residue(Ts[i]), Tlabels[i]))
-    allmats.append(colpermcheck(Ts[i]))
+txt = open("patterns.txt","w", encoding="utf-8")
 
-for j in range(1,4):
-    print(j)
+for i in range(len(Ts)):
+    tcountpatterns[0].append((Ts[i], Tlabels[i]))
+    allmats.append(colpermcheck(Ts[i]))
+    txt.write(fileoutput(Ts[i], 1, Tlabels[i]))
+
+for j in range(1,5):
     for i in range(len(Ts)):
         for tup in tcountpatterns[j-1]:
             # Checks if it's not a column permutation
             if colpermcheck(matmult(tup[0],Ts[i])) not in allmats:
                 # Checks if it's not a clifford matrix
                 if not CliffCheck(matmult(tup[0],Ts[i])):
-                    tcountpatterns[j].append((matmult(tup[0],Ts[i]), j+1, residue(matmult(tup[0],Ts[i])), tup[3]+Tlabels[i]))
-                    allmats.append(colpermcheck(matmult(tup[0],Ts[i])) )
+                    tcountpatterns[j].append((matmult(tup[0],Ts[i]), tup[1]+Tlabels[i]))
+                    allmats.append(colpermcheck(matmult(tup[0],Ts[i])))
+                    txt.write(fileoutput(tup[0],j+1, tup[1]+Tlabels[i]))
 
 for row in tcountpatterns:
     print(len(row))
-
-##print(matmult(Tmat(1,2),Tmat(3,4)))
-##print(matmult(Tmat(3,4),Tmat(1,2)))
-##
-##print(matmult(Tmat(1,1),Tmat(1,1)))
