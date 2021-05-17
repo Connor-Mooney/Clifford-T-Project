6b5d7b51bb0eb3610d4113d6be1d17121fbabd9b
Deranged-Alpacas <derangedalpacas1234@gmail.com>
2021-05-16 22:59:51 -0400

    Refactoring to enable parallelization

--- TOperators/main.cpp
+++ TOperators/main.cpp
@@ -75,6 +75,20 @@ std::vector<SO6> genPerms(std::vector<SO6>& oneLDE, int LDE, std::vector<std::ve
     return(toReturn);
 }
 
+std::vector<std::vector<SO6>> mergedVect(std::vector<std::vector<SO6>>& a, std::vector<std::vector<SO6>>& b){
+    std::vector<std::vector<SO6>> m;
+    std::vector<SO6> u;
+    for(int i = 0; i<a.size(); i++){
+        u = std::vector<SO6>();
+        for(int j = 0; j<a[i].size(); j++)
+            u.push_back(a[i][j]);
+        for(int j = 0; j<b[i].size(); j++)
+            u.push_back(b[i][j]);
+        m.push_back(u);
+    }
+    return(m);
+}
+
 int main(){
     //generating list of T matrices
     //in the order Andrew wanted
@@ -108,47 +122,52 @@ int main(){
     std::cout<<"LDE 1:"<<t2[1].size()<<"\n";
     std::cout<<"LDE 2:"<<t2[2].size()<<"\n";
     evens.push_back(t2);
-//    //generating t count 3
-//    std::vector<SO6> t3[4];
-//    for(int j = 0; j<3; j++){
-//        for(int i = 0; i<15; i++){
-//            for(SO6 m : t2[j]){
-//                prod = ts[i]*m;
-//                int LDEIndex = prod.getLDE();
-//                if((!containedIn(ts, prod))&&(!containedIn(t3[LDEIndex],prod)))
-//                    t3[LDEIndex].push_back(prod);
-//            }
-//        }
-//    }
-//    std::cout<<"Generated T Count 3 \n";
-//    std::cout<<"LDE 0:"<<t3[0].size()<<"\n";
-//    std::cout<<"LDE 1:"<<t3[1].size()<<"\n";
-//    std::cout<<"LDE 2:"<<t3[2].size()<<"\n";
-//    std::cout<<"LDE 3:"<<t3[3].size()<<"\n";
-//
-//
-//    //generating t count 4
-//    //this step takes something in the order of 100 times longer,
-//    //Reduced runtime including this to ~3 minutes, but it goes from 3 seconds to get to T-Count 3
-//    //to that for 4... not a good growth rate
-//    std::vector<SO6> t4[5];
-//    for(int j = 0; j<4; j++){
-//        for(int i = 0; i<15; i++){
-//            std::cout<<i<<"\n";
-//            for(SO6 m : t3[j]){
-//                prod = ts[i]*m;
-//                int LDEIndex = prod.getLDE();
-//                if((!containedIn(t2[LDEIndex],prod))&&(!containedIn(t4[LDEIndex],prod)))
-//                    t4[LDEIndex].push_back(prod);
-//            }
-//        }
-//    }
-//    std::cout<<"Generated T Count 4 \n";
-//    std::cout<<"LDE 0:"<<t4[0].size()<<"\n";
-//    std::cout<<"LDE 1:"<<t4[1].size()<<"\n";
-//    std::cout<<"LDE 2:"<<t4[2].size()<<"\n";
-//    std::cout<<"LDE 3:"<<t4[3].size()<<"\n";
-//    std::cout<<"LDE 3:"<<t4[4].size()<<"\n";
+    //generating t count 3
+    std::vector<std::vector<SO6>> t3(4);
+    std::vector<std::vector<SO6>> prods[4];
+    prods[0] = genProds(t2[0], 2, ts);
+    prods[1] = genProds(t2[1], 2, ts);
+    prods[2] = genProds(t2[2], 2, ts);
+    //some sort of error here
+    std::vector<std::vector<SO6>> merg(4);
+    merg = mergedVect(prods[0], prods[1]);
+    t3 = mergedVect(merg, prods[2]);
+    t3[0] = genPerms(t3[0], 0, odds);
+    t3[1] = genPerms(t3[1], 1, odds);
+    t3[2] = genPerms(t3[2], 2, odds);
+    t3[3] = genPerms(t3[3], 3, odds);
+    odds.push_back(t3);
+
+    std::cout<<"Generated T Count 3 \n";
+    std::cout<<"LDE 0:"<<t3[0].size()<<"\n";
+    std::cout<<"LDE 1:"<<t3[1].size()<<"\n";
+    std::cout<<"LDE 2:"<<t3[2].size()<<"\n";
+    std::cout<<"LDE 3:"<<t3[3].size()<<"\n";
+
+
+
+    //generating t count 4
+    //this step takes something in the order of 100 times longer,
+    //Reduced runtime including this to ~3 minutes, but it goes from 3 seconds to get to T-Count 3
+    //to that for 4... not a good growth rate
+    std::vector<std::vector<SO6>> t4(5);
+    prods[0] = genProds(t3[0], 3, ts);
+    prods[1] = genProds(t3[1], 3, ts);
+    prods[2] = genProds(t3[2], 3, ts);
+    prods[3] = genProds(t3[3], 3, ts);
+    merg = mergedVect(prods[0], prods[1]);
+    merg = mergedVect(merg,prods[2]);
+    t4   = mergedVect(merg, prods[3]);
+    t4[0]= genPerms(t4[0], 0, evens);
+    t4[1]= genPerms(t4[1], 1, evens);
+    t4[2]= genPerms(t4[2], 2, evens);
+    t4[3]= genPerms(t4[3], 3, evens);
+    std::cout<<"Generated T Count 4 \n";
+    std::cout<<"LDE 0:"<<t4[0].size()<<"\n";
+    std::cout<<"LDE 1:"<<t4[1].size()<<"\n";
+    std::cout<<"LDE 2:"<<t4[2].size()<<"\n";
+    std::cout<<"LDE 3:"<<t4[3].size()<<"\n";
+    std::cout<<"LDE 4:"<<t4[4].size()<<"\n";
 
 
 
